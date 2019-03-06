e3814b1a0d1c35b97a1eb6947524fe0e273c393e
Андрей Рогацкий <cactus@bk.ru>
2019-03-07 01:24:27 +0300

    use pep8

--- homeworks/homework_01/hw1_invertint.py
+++ homeworks/homework_01/hw1_invertint.py
@@ -3,14 +3,14 @@
 
 
 def reverse(number):
-    isNegative = number<0
+    result = 0
+    isNegative = number < 0
 
     if isNegative:
         number = -number
 
-    result = 0
     while number != 0:
-        result = result*10 + number%10
+        result = result*10 + number % 10
         number = number // 10
 
     if isNegative:
